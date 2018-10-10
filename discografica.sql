ea229fcded36443298efe79c7d9490e25291982f
David Rabassa <gestionarlaweb@gmail.com>
2018-10-10 15:29:32 +0200

    Base de datos con la nueva tabla usuarios

--- discografica.sql
+++ discografica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-09-2018 a las 09:17:16
+-- Tiempo de generación: 10-10-2018 a las 15:26:25
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -41,7 +41,7 @@ CREATE TABLE `artistas` (
 INSERT INTO `artistas` (`id`, `nombre`, `descripcion`) VALUES
 (1, 'Shakira', 'Cantautora, productora discográfica, bailarina, modelo, empresaria, actriz y embajadora de buena voluntad de la UNICEF y filántropa colombiana, ha sido nombrada varias veces por Sony y Billboard con el sobrenombre de La Reina del pop latino.'),
 (2, 'Bruce Springsteen', 'Cantante, músico y compositor estadounidense. Apodado a menudo The Boss —en español: El jefe—, Springsteen es ampliamente conocido por su trabajo con el grupo The E Street Band y considerado uno de los artistas más exitosos de la música rock, con ventas que superan los 64,5 millones de álbumes en los Estados Unidos y más de 120 millones a nivel mundial,4​5​ y un total de diez discos números uno, un registro solo superado por The Beatles y Jay-Z'),
-(3, 'Madona', 'Cantante, compositora, actriz y empresaria estadounidense. Pasó sus primeros años en Bay City y en 1977 se mudó a la ciudad de Nueva York para realizar una carrera de danza contemporánea'),
+(3, 'Madonna', 'Cantante, compositora, actriz y empresaria estadounidense. Pasó sus primeros años en Bay City y en 1977 se mudó a la ciudad de Nueva York para realizar una carrera de danza contemporánea'),
 (4, 'Michael Jackson', 'Fue un cantante, compositor, productor discográfico, bailarín, actor y filántropo estadounidense.2​3​4​ Conocido como el «Rey del Pop»,5​ sus contribuciones y reconocimiento en la historia de la música y el baile, así como su publicitada vida personal, lo convirtieron en una figura internacional en la cultura popular durante más de cuatro décadas.'),
 (5, 'David Bowie', 'Más conocido por su nombre artístico David Bowie (AFI: [ˈboʊ.iː]),8​ fue un músico y compositor británico de rock, quien ejerció a su vez de actor, productor discográfico y arreglista. Figura importante de la música popular durante casi cinco décadas, Bowie es considerado un innovador, en particular por sus trabajos de la década de 1970 y por su peculiar voz, además de la profundidad intelectual de su obra.'),
 (6, 'Bob Dylan', 'Registrado al nacer como Robert Allen Zimmerman (en hebreo: שבתאי זיסל בן אברהם, Shabtai Zisl ben Avraham), es un músico, compositor, cantante y poeta estadounidense, ampliamente considerado como una de las figuras más prolíficas e influyentes en la música popular del siglo XX y de comienzos del siglo XXI');
@@ -69,7 +69,27 @@ INSERT INTO `lp` (`id`, `id_artista`, `nombre`, `descripcion`) VALUES
 (3, 6, 'Bob Dylan', 'Primer álbum de estudio del músico estadounidense.'),
 (4, 6, 'The Freewheelin\' Bob Dylan', 'Segundo álbum de estudio del músico estadounidense Bob Dylan, publicado por la compañía discográfica Columbia Records en mayo de 1963. Tras empezar su carrera versionando y adaptando temas tradicionales de su país, The Freewheelin\' supuso el comienzo de la carrera compositiva de Dylan al presentar once composiciones propias'),
 (5, 4, 'The Wiz', 'En el año 1975 la obra obtuvo un total de siete Premios Tony incluyendo Mejor Musical'),
-(6, 4, 'Moonwalker', 'Moonwalker, también conocida como Michael Jackson: Moonwalker, es una película del género de antología musical del artista estadounidense Michael Jackson, lanzada en octubre de 1988.');
+(6, 4, 'Moonwalker', 'Moonwalker, también conocida como Michael Jackson: Moonwalker, es una película del género de antología musical del artista estadounidense Michael Jackson, lanzada en octubre de 1988.'),
+(7, 4, 'Invincible', 'Décimo primer y último álbum de estudio en vida de Michael Jackson lanzado el 30 de octubre de 2001.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(15) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `password`) VALUES
+(1, 'davidrabassa@gmail.com', '1234');
 
 --
 -- Índices para tablas volcadas
@@ -88,6 +108,12 @@ ALTER TABLE `lp`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -101,7 +127,13 @@ ALTER TABLE `artistas`
 -- AUTO_INCREMENT de la tabla `lp`
 --
 ALTER TABLE `lp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
